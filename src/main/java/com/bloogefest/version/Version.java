2ae30f976862d251d53f42893ce66438a9a9138b
Bloogefest <bloogefest@gmail.com>
2023-07-16 01:29:31 +0300

    Введение интерфейса `Version`

---
+++ src/main/java/com/bloogefest/version/Version.java
@@ -0,0 +1,60 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.bloogefest.version;
+
+import com.bloogefest.annotation.Contract;
+import com.bloogefest.annotation.NotNull;
+import com.bloogefest.common.validation.NullException;
+
+/**
+ * Этот интерфейс содержит методы, применимые к любому формату версий.
+ *
+ * @see #newer(Version)
+ * @see #same(Version)
+ * @see #older(Version)
+ * @since 1.0.0-RC1
+ */
+public interface Version {
+
+    /**
+     * Возвращает истинное значение, если переданная версия новее текущей, в противном случае — ложное значение.
+     *
+     * @return Истинное или ложное значение.
+     *
+     * @see #same(Version)
+     * @see #older(Version)
+     * @since 1.0.0-RC1
+     */
+    @Contract("!null -> _; _ -> fail")
+    boolean newer(final @NotNull Version version) throws NullException;
+
+    /**
+     * Возвращает истинное значение, если переданная версия такая же, как и текущая, в противном случае — ложное
+     * значение.
+     *
+     * @return Истинное или ложное значение.
+     *
+     * @see #newer(Version)
+     * @see #older(Version)
+     * @since 1.0.0-RC1
+     */
+    @Contract("!null -> _; _ -> fail")
+    boolean same(final @NotNull Version version) throws NullException;
+
+    /**
+     * Возвращает истинное значение, если переданная версия старее текущей, в противном случае — ложное значение.
+     *
+     * @return Истинное или ложное значение.
+     *
+     * @see #newer(Version)
+     * @see #same(Version)
+     * @since 1.0.0-RC1
+     */
+    @Contract("!null -> _; _ -> fail")
+    boolean older(final @NotNull Version version) throws NullException;
+
+}
