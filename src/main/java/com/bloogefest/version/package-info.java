1dc78f883b04ece6789fe324d17a3d5d135d9735
Bloogefest <bloogefest@gmail.com>
2023-07-16 01:29:12 +0300

    Введение пакета `com.bloogefest.version`

---
+++ src/main/java/com/bloogefest/version/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/**
+ * Этот пакет содержит базовые инструменты для работы версиями и их форматами.
+ *
+ * @since 1.0.0-RC1
+ */
+package com.bloogefest.version;
